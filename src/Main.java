dc449b20bde34e5d9480b7413d51ecec5b338164
Vlad <vladvahturov@yandex.ru>
2023-10-13 14:53:57 +0300

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args)
     {
-        int a = 1;
-        int b = 2;
-        System.out.println(a + b);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ВВЕДИТЕ ПЕРВОЕ ЧИСЛО");
+        int a = scanner.nextInt();
+        System.out.println("ВВЕДИТЕ ВТОРОЕ ЧИСЛО");
+        int b = scanner.nextInt();
+
+        System.out.println("ВВЕДИТЕ ОПЕРАЦИЮ");
+        char s = scanner.next().charAt(0);
+        switch (s){
+            case('+'):
+                System.out.println(a+b);
+                break;
+            case('-'):
+                System.out.println(a-b);
+                break;
+            case('*'):
+                System.out.println(a*b);
+                break;
+            case('/'):
+                System.out.println(a/b);
+                break;
+        }
     }
 }
